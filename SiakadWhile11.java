48ac7aa3c42da8365b0b686155fa2e300c83eb9a
Valencza <143587759+Valencza@users.noreply.github.com>
2025-10-16 08:45:48 +0700

    Modifikasi While

--- SiakadWhile11.java
+++ SiakadWhile11.java
@@ -5,9 +5,9 @@ public class SiakadWhile11 {
         Scanner sc = new Scanner(System.in);
         
         int jml;
-        System.out.print("Masukkan jumlah mahasiswa: ");
+        System.out.print("Masukkan jumlah Mahasiswa: ");
         jml = sc.nextInt();
-        sc.nextLine(); // Membersihkan buffer
+        sc.nextLine(); 
         
         int i = 0;
         
@@ -16,7 +16,7 @@ public class SiakadWhile11 {
             
             if (sc.hasNextDouble()) {
                 double nilai = sc.nextDouble();
-                sc.nextLine(); // Membersihkan buffer
+                sc.nextLine();
                 
                 if (nilai < 0 || nilai > 100) {
                     System.out.println("Nilai tidak valid. Masukkan lagi nilai yang valid:");
@@ -25,6 +25,7 @@ public class SiakadWhile11 {
                 
                 if (nilai >= 80 && nilai <= 100) {
                     System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah A");
+                    System.out.println("Bagus, pertahankan nilainya!");
                 } else if (nilai >= 73 && nilai < 80) {
                     System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B+");
                 } else if (nilai >= 65 && nilai < 73) {
@@ -39,10 +40,10 @@ public class SiakadWhile11 {
                     System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah E");
                 }
                 
-                i++; // Hanya increment jika input valid
+                i++; 
             } else {
                 System.out.println("Input tidak valid! Masukkan angka.");
-                sc.nextLine(); // Membersihkan input yang tidak valid
+                sc.nextLine(); 
             }
         }
         
